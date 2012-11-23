cc06961669eabc867fa75329f4e00a8dca3bcafd
Ricardo Pérez (Sothoth) <ricpelo@gmail.com>
2012-11-24 00:50:27 +0100

    mMás ejoras en el código de ExaminarFalso y menús de accioness

--- Alpha_pack/ExaminarFalso.h
+++ Alpha_pack/ExaminarFalso.h
@@ -52,6 +52,7 @@ Object ExaminarFalso
   with
     objetoVerboDesconocido 0,   ! El objeto sobre el que actuar
     palabraMenu 0,              ! Se usa para los menús de acciones
+    mostrarExaminar false,      ! ¿Hay que mostrar "> ex objeto"?
     EF_UnknownVerb [x
       obj;
       ! Se busca en todos los objetos que están al alcance, incluyendo la
